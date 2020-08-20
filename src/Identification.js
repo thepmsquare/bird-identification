7e598d6a834319d37a9dfdba7679a475c3ba1e8b
thePmSquare <impmmmm@gmail.com>
2020-08-20 15:43:13 +0530

    add searchbar (not functional)

--- src/Identification.js
+++ src/Identification.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import PhotoCameraIcon from "@material-ui/icons/PhotoCamera";
 import "./stylesheets/Identification.css";
 
 class Identification extends Component {
   render = () => {
     return (
       <div className="Identification">
-        <h1>Identification</h1>
+        <Typography variant="h2" component="h2">
+          Bird Identification
+        </Typography>
+        <form noValidate autoComplete="off">
+          <TextField
+            id="outlined-basic"
+            label="Search"
+            variant="outlined"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="start">
+                  <IconButton>
+                    <PhotoCameraIcon />
+                  </IconButton>
+                  <IconButton>
+                    <SearchIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </form>
       </div>
     );
   };
