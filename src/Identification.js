c7e28b9105f9a7b5000663d10c31335b893629a3
thePmSquare <impmmmm@gmail.com>
2020-08-20 14:05:49 +0530

    initial set up

---
+++ src/Identification.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./stylesheets/Identification.css";
+
+class Identification extends Component {
+  render = () => {
+    return (
+      <div className="Identification">
+        <h1>Identification</h1>
+      </div>
+    );
+  };
+}
+
+export default Identification;
